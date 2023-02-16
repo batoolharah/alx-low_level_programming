1fe3015225b7e4a0a1c6ee9e8634b98041fd9ec5
batoolharah <batooladnanharah@gmail.com>
2023-02-16 06:58:10 -0800

    my first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Prints "with proper grammar , but the outcome is as piece of art"
+ *       
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf('with proper grammar, but the outcome is a piece of art,');
+	printf('with proper grammar, but the outcome is a piece of art,\n');
 
 	return (0);
 }
