6447b756f00049a85fb5f98016d33a3c3b3ecf31
batoolharah <batooladnanharah@gmail.com>
2023-02-17 10:24:18 -0800

    my first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,10 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
-		if (num != 9)
-		{
-		  putchar(',');	
-		  putchar(' ');
-		}
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
