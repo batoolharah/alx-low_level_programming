93ff5864af51db4ca4364ac713cf4b4604a989b7
batoolharah <batooladnanharah@gmail.com>
2023-02-21 09:15:48 -0800

    my first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+	return (abs(ab));
+}
