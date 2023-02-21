efa1a9da13a76e492d5947bcf969f07983f611ab
batoolharah <batooladnanharah@gmail.com>
2023-02-21 08:48:07 -0800

    my first commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - a function that hecks for lowercase character
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
