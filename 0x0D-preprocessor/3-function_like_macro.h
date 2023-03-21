96e120e1eb1df296194ec2a7b9bafac090483e88
batoolharah <batooladnanharah@gmail.com>
2023-03-20 17:39:39 -0700

    my fisrt commitit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef _function_like_macro_h_
-#define _function_like_macro_h_
-
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
-
+#ifndef ABS_H
+#define ABS_H
+#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
 #endif 
